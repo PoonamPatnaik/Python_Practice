492f2c75b45655870033e6007f841355d0816e29
poonam_qa <khusi.poo@gmail.com>
2024-03-02 15:36:00 +0530

    added file

--- automate_boring_stuff/first_program.py
+++ automate_boring_stuff/first_program.py
@@ -3,4 +3,4 @@ u_name = input("Please enter you name")
 print("Hey " + u_name + " Let's Code")
 time_spent = int(input("How many hours you can practise in a day"))
 month_spent = time_spent * 30
-print("Total effort in a month would be " + str(month_spent) +" hr")
+print("Total effort in a month would be " + str(month_spent) + " hrs")
