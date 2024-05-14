d619c0664375da3a71ea8df2b69d84587916fb4a
poonam_qa <khusi.poo@gmail.com>
2024-05-14 07:19:53 +0530

    added string reversal code

--- automate_boring_stuff/reverse_str.py
+++ automate_boring_stuff/reverse_str.py
@@ -1,34 +1,4 @@
 
-def while_reverse_str(name):
-    str_len = len(name)
-    rev_name = ''
-    while str_len > 0:
-        rev_name = rev_name + name[str_len-1]
-        str_len = str_len-1
-    print("Reverse string is ",rev_name)
-def for_reverse_str(name):
-    rev_name = ''
-    for cha in name:
-        rev_name = cha + rev_name
-    print("Reverse string is ",rev_name)
-
-def silcing_reverse_str(name):
-    rev_name = name[::-1]
-    print("Reverse string is ",rev_name)
-
-name = input("Enter the Name")
-print("Actual name is :" + name)
-# 3 ways to reverse a string: while, for loop, slicing
-#while_reverse_str(name)
-#for_reverse_str(name)
-silcing_reverse_str(name)
-
-
-
-
-
-
-'''
 name = input("Enter the Name")
 print("Actual name is :" + name)
 r_name = ""
@@ -57,5 +27,5 @@ def rev_string_m(str_len):
     return rev_name
 reversed_nname = rev_string_m(new_len_strg)
 print("reverse name is: " + reversed_nname)
-'''
+
 
--- automate_boring_stuff/str_func.py
+++ automate_boring_stuff/str_func.py
@@ -1,14 +1,16 @@
+name = input("Enter name")
 
-def rev_str(len_strg):
+def rev_str(actual_string):
     rev_name = ""
+    len_strg = len(actual_string)
     for i in range(len_strg):
         print(i,len_strg-i)
         rev_name = rev_name + name[len_strg - i - 1]
         print(rev_name)
     return rev_name
-reversed_name = rev_str(len_strg)
+reversed_name = rev_str(name)
 print("reverse name is: " + reversed_name)
-
+new_name = input("Enter name")
 new_len_strg = len(new_name)
 def rev_string_m(str_len):
     rev_name = ""
