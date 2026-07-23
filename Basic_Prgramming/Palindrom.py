ffedbc6b7af2738def9c43b9820a7bfe0f24ffa0
poonam_qa <khusi.poo@gmail.com>
2026-07-23 06:20:46 +0530

    added more function

--- Basic_Prgramming/Palindrom.py
+++ Basic_Prgramming/Palindrom.py
@@ -3,5 +3,18 @@ def verify_palindrom(str_copy):
         print ("YES")
     else:
         print ("NO")
-str_copy = (input("enter a string"))
-verify_palindrom(str_copy)
+def check_palidrom(str_copy):
+    reversed_str =""
+    leng_str = len(str_copy)
+    for i in range(leng_str):
+        reversed_str = reversed_str + str_copy[leng_str - i - 1]
+    if str_copy == reversed_str:
+        print ("YES")
+    else:
+        print ("NO")
+        print(reversed_str)
+
+
+
+str_copy = (input("enter a string  "))
+check_palidrom(str_copy)
