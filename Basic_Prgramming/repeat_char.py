45b4adf8a9ec0fb8cab5759881ec4ad59c442061
poonam_qa <khusi.poo@gmail.com>
2026-08-01 07:53:53 +0530

    code In progress

--- Basic_Prgramming/repeat_char.py
+++ Basic_Prgramming/repeat_char.py
@@ -1,18 +1,21 @@
 word = input("Enter the word")
 char = input("Enter the char")
-wl = len(word)
-count = 0
-'''for i in range(wl):
-    if word[i] == char:
-        count = count +1
-print(char,count)'''
 
-
-
-for item in word:
-    if item == char:
-        count = count + 1
-print(char,count)
-
+def check_char_occurance(word,char):
+    count = 0
+    for i in range(len(word)):
+        if word[i] == char:
+            count = count +1
+    return (char,count)
+def each_char_occurance(word,char):
+    my_tuple =()
+    count = 0
+    for i in range(len(word)):
+        if word[i] not in my_tuple:
+            my_tuple = (word[i],1)
+        else:
+            my_tuple = (word[i],my_tuple[1]+1)
+    return my_tuple
+print(each_char_occurance(word,char))
 
 
